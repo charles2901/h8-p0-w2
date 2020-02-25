bf2c8e2dd1746e44207453ab9757efbcbda014fe
charles2901 <61080364+charles2901@users.noreply.github.com>
2020-02-25 09:19:07 +0700

    Update exercise3.js
    edit exercise3

--- exercise3.js
+++ exercise3.js
@@ -22,15 +22,15 @@ if(nama == ''){
 else if(peran ==''){
   console.log("Halo " + nama+ ", Pilih peranmu untuk memulai game!");
 }
-else if(peran == "Ksatria"){
+else if(peran == 'Ksatria'){
   console.log("Selamat datang di Dunia Proxytia, "+nama);
   console.log("Halo Ksatria " +nama +", kamu dapat menyerang dengan senjatamu!")
 }
-else if(peran == "Tabib"){
+else if(peran == 'Tabib'){
   console.log("Selamat datang di Dunia Proxytia, "+nama);
   console.log("Halo Tabib "+ nama +", kamu akan membantu temanmu yang terluka.");
 }
-else if(peran == "Penyihir"){
+else if(peran == 'Penyihir'){
   console.log("Selamat datang di Dunia Proxytia, "+nama);
   console.log("Halo Penyihir "+ nama +", ciptakan keajaiban yang membantu kemenanganmu!");
 }
